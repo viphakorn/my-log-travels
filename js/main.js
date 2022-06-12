209d0eafa7c5eba8a45c83f0760d33ec4359d9e2
viphakorn <viphakone585@gmail.com>
2022-06-12 20:14:12 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// const slide = document.querySelector("#client .client .slide");
+// const card = document.querySelectorAll("#client .client .slide .card");
+// const next = document.querySelector("#client .client .slide #next")
+// const prev = document.querySelector("#client .client .slide #prev")
+
+
+// prev.onclick = () => {
+//     card[0].style.transform = "translateX(100%)"
+//     card[1].style.transform = "translateX(100%)"
+//     card[2].style.transform = "translateX(100%)"
+// }
+
+// next.onclick = () => {
+//     card[0].style.transform = "translateX(-100%)"
+//     card[1].style.transform = "translateX(-100%)"
+//     card[2].style.transform = "translateX(-100%)"
+// }
